20514d81a30d147a56fe3b6490d3c026df56cfba
Fabien O'Carroll <fabien@allou.is>
2014-04-10 20:26:56 +0100

    broadcast msg when recieved

--- server/index.js
+++ server/index.js
@@ -17,8 +17,7 @@ function handler (req, res) {
 }
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-      console.log(data);
-    });
+  socket.on('msg', function(msg) {
+    io.sockets.emit('msg', msg);
+  });
 });
